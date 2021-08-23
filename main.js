170fed91c81c0fbed5785bbf46b24b8a89cbe20f
Adam Falchetta <adamf1631@yahoo.com>
2021-08-23 11:48:48 -0400

    Add files via upload

---
+++ main.js
@@ -0,0 +1,35 @@
+//.toString
+// const arr = [1,2,3,'a','b','c'];
+// const num = 5;
+
+// console.log(arr.toString())
+
+//.pop()
+// const arr = [1,2,3,'a','b','c'];
+// const newArr = arr.pop()
+
+// console.log(newArr)
+
+//.reverse
+// const arr = [1,2,3,'a','b','c'];
+
+// console.log(arr.reverse())
+
+//.indexOf()
+
+// const arr = ['x','y','z'];
+
+// console.log(arr.indexOf('z'));
+
+//.search()
+// let word = 'Adam Smith';
+// console.log(word.search('Smith'));
+
+//.replace()
+// let greeting = 'Hello World!!';
+
+// console.log(
+//   greeting,
+//   greeting.replace('World!!', 'Everyone')
+// );
+
